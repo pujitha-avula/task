9aa67cb4b7418bb57bd2d847850652c74b15e0f7
pujitha-avula <vyshuuyadav@gmail.com>
2025-07-23 12:48:22 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,19 @@
+
+# for i in list[2,5,6,7,8,8,8,9,9,9,9,6,65,55,66]:
+#      print(i) 
+# for i in range(0,8):
+#     print(i)
+# for i in tuple(a,s,d,f):
+#     print(i)
+
+num=int(input('enter a value'))
+i=0
+while True:
+    print(num*i)
+    i+=1
+    if i==12:
+        break
+     
+    
+#  print('anitha oo anitha')
+# print('na pranamaaa'
